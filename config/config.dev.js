756888562711e5f61153bdee36570f5898d781fa
J. Nolan Hager <jeffrey.hager.dev@gmail.com>
2016-04-13 21:49:35 -0500

    Adding a missing file

---
+++ config/config.dev.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "openDevTools": true,
+  "board":{
+    "size": {'x': 15, 'y': 25}
+  },
+  "window": { // This is just the option object for electron
+    "width": 800,
+    "height": 600
+  }
+};
